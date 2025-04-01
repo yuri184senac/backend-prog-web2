4d2d02a493b23f78ea54e8ff5e4fdb33aeef1c55
ADS 2023.2N <yuri>
2025-04-01 19:01:06 -0300

    l

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,7 +1,7 @@
 import { cert, initializeApp } from "firebase-admin/app";
 
 export function InitializeFirebase() {
-    const serviceAccount: string = 'src/keys/serviceinfo.json';
+    const serviceAccount: string = 'src/keys/serviceinfe.json';
     initializeApp({
         credential: cert(serviceAccount)
     });
