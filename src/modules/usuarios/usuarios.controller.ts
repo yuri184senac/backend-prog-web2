f76101ab5359ba86b06b3e5dcda4026197233061
ADS 2023.2N <yuri>
2025-04-01 21:46:01 -0300

    Dok

--- src/modules/usuarios/usuarios.controller.ts
+++ src/modules/usuarios/usuarios.controller.ts
@@ -1,15 +1,40 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import { UsuariosService } from './usuarios.service';
+import { Usuario } from 'src/models/usuario.interface';
 
 @Controller('usuarios')
 export class UsuariosController {
+
+    constructor(private usuarioService: UsuariosService) { }
     //Pegar usuario
     @Get('/:id')
-    public getUserById(@Param() params: string @Body() ) {
-
+    public getUserById(@Param() params: any) {
+        try {
+            return this.usuarioService.getUserById(params.id);
+        } catch (err) {
+            throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR, err);
+        }
     }
 
     //Criar usuario
+    @Post('')
+    public createUser(@Body() body: Usuario) {
+        try {
+            return this.usuarioService.createUser(body);
+        } catch (err) {
+            throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR, err);
+        }
+    }
+
     //Editar usuario
+    @Put('/:id')
+    public updateUserById(@Param() params: any, @Body() body: Usuario) {
+        try {
+            return this.usuarioService.updateUserById(params.id, body);
+        } catch (err) {
+            throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR, err);
+        }
+    }
     //Excluir usuario
     
 }
--- src/modules/usuarios/usuarios.service.ts
+++ src/modules/usuarios/usuarios.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { FirestoreService } from 'src/core/service/firestore-service.service';
+import { Usuario } from 'src/models/usuario.interface';
 
 @Injectable()
 export class UsuariosService {
     constructor(private firestoreService: FirestoreService) { }
 
-    getUserById(path: string) {
+    getUserById(id: string) {
+        const path = `usuarios/${id}`;
         return this.firestoreService.getDocument(path);
+    }
+
+    createUser(usuario: Usuario) {
+        const path = `usuarios`;
+        return this.firestoreService.createDocument(path, usuario);
+    }
+
+    updateUserById(id: string, usuario: Usuario) {
+        const path = `usuarios/${id}`;
+        return this.firestoreService.updateDocument(path, usuario);
+    }
+
+    deleteUserById(id: string) {
+        const path = `usuarios/${id}`;
+        return this.firestoreService.deleteDocument(path);
     }
 }
