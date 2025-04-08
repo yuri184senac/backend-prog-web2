5025c17bee0cd8ab88a67ce04c93a099741a8043
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2025-04-08 19:32:27 -0300

    ajustes nos controlleres

--- src/modules/cursos/cursos.controller.ts
+++ src/modules/cursos/cursos.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpException, HttpStatus } from '@nestjs/common';
 
 @Controller('cursos')
-export class CursosController {}
+export class CursosController {
+    private async handleRequest(promise: Promise<any>) {
+        try {
+            return await promise;
+        } catch (err) {
+            throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR, err);
+        }
+    }
+}
